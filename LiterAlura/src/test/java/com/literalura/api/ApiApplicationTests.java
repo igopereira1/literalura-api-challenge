8c1029b2d5c38e2eaa52bdf1c1b6c74709dc91db
igopereira1 <igopereira1@gmail.com>
2024-06-28 22:35:06 -0300

    feat: Initialize Spring project

---
+++ LiterAlura/src/test/java/com/literalura/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.literalura.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
