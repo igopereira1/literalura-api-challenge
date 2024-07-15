641980f0fbde8e7891e1ab7813b2c1d56a229943
igopereira1 <igopereira1@gmail.com>
2024-07-15 19:51:44 -0300

    feat: add findByLanguageContainingIgnoreCase method to BookRepository

--- LiterAlura/src/main/java/com/literalura/api/repository/BookRepository.java
+++ LiterAlura/src/main/java/com/literalura/api/repository/BookRepository.java
@@ -2,8 +2,12 @@ package com.literalura.api.repository;
 
 import com.literalura.api.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findByTitleContainingIgnoreCase(String bookTitle);
+
+    List<Book> findByLanguageContainingIgnoreCase(String language);
 }
