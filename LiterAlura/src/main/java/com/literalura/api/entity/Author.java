52222cd6aae4f58cd2327f86861699852f3dcb12
igopereira1 <igopereira1@gmail.com>
2024-07-15 10:57:52 -0300

    feat: add JPA entities for Author and Book

---
+++ LiterAlura/src/main/java/com/literalura/api/entity/Author.java
@@ -0,0 +1,53 @@
+package com.literalura.api.entity;
+
+import com.literalura.api.dto.AuthorDataDTO;
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "authors")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String name;
+    private Integer birthYear;
+    private Integer deathYear;
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Book> books = new ArrayList<>();
+
+    public Author() {
+    }
+
+    public Author(AuthorDataDTO authorDataDTO) {
+        this.name = authorDataDTO.name();
+        this.birthYear = authorDataDTO.birthYear();
+        this.deathYear = authorDataDTO.deathYear();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getBirthYear() {
+        return birthYear;
+    }
+
+    public Integer getDeathYear() {
+        return deathYear;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthYear(Integer birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public void setDeathYear(Integer deathYear) {
+        this.deathYear = deathYear;
+    }
+}
---
+++ LiterAlura/src/main/java/com/literalura/api/entity/Book.java
@@ -0,0 +1,66 @@
+package com.literalura.api.entity;
+
+import com.literalura.api.dto.BookDataDTO;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
+    private String language;
+    private Integer downloads;
+
+    public Book() {}
+
+    public Book(BookDataDTO bookDataDTO) {
+        this.title = bookDataDTO.title();
+        this.language = String.join(" ", bookDataDTO.languages());
+        this.downloads = bookDataDTO.downloadCount();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public Integer getDownloads() {
+        return downloads;
+    }
+
+    public void setDownloads(Integer downloads) {
+        this.downloads = downloads;
+    }
+}
