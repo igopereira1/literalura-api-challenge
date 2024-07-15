973387bedceb89c2d0d7490645e29c458b21e0f1
igopereira1 <igopereira1@gmail.com>
2024-07-15 14:36:52 -0300

    refactor: integrate main application logic into Spring Boot lifecycle

--- LiterAlura/src/main/java/com/literalura/api/ApiApplication.java
+++ LiterAlura/src/main/java/com/literalura/api/ApiApplication.java
@@ -1,13 +1,23 @@
 package com.literalura.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.literalura.api.main.Main;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication implements CommandLineRunner {
+	@Autowired
+	private Main main;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws JsonProcessingException {
+		main.showMenu();
+	}
 }
