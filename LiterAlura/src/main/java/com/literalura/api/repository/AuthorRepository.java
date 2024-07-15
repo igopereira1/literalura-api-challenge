456c4ea1fbc1d00dcea485a4f2f8a85edec343c2
igopereira1 <igopereira1@gmail.com>
2024-07-15 19:32:03 -0300

    feat: added findAuthorsAliveInYear method to AuthorRepository

--- LiterAlura/src/main/java/com/literalura/api/repository/AuthorRepository.java
+++ LiterAlura/src/main/java/com/literalura/api/repository/AuthorRepository.java
@@ -2,8 +2,13 @@ package com.literalura.api.repository;
 
 import com.literalura.api.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 import java.util.Optional;
 
 public interface AuthorRepository extends JpaRepository<Author, Long>{
     Optional<Author> findByNameEqualsIgnoreCase(String authorName);
+
+    @Query("SELECT a FROM Author a WHERE :year BETWEEN a.birthYear AND a.deathYear")
+    List<Author> findAuthorsAliveInYear(Integer year);
 }
